574372d80aeb907880de83573e3fdbdeb68fdd17
narsingh24 <narsingh@iclimbs.com>
2025-08-05 02:25:51 +0530

    localhost testing done

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 // server.js
-const app = require('./api/clone'); // Reuse the same Express app
+const app = require('./api/clone'); // reuse Express app
+const express = require('express');
+const path = require('path');
+
 const PORT = 3000;
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html on root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
